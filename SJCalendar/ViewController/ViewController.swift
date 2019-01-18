e3353aa1623ffa6f44586a20d83522d8ffc95e74
byunsangjin <qustkdwls1@naver.com>
2019-01-18 12:49:24 +0900

    init Project

---
+++ SJCalendar/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SJCalendar
+//
+//  Created by Byunsangjin on 18/01/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
